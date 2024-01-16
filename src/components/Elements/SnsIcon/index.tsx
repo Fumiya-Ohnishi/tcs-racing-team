57fb283bc6f68473995652430bafe0319a4aeab3
Fumiya-Ohnishi <soccer.14.2.25@gmail.com>
2024-01-17 03:31:31 +0900

    Official Media

---
+++ src/components/Elements/SnsIcon/index.tsx
@@ -0,0 +1,19 @@
+import { Box, Image } from '@chakra-ui/react';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  href: string;
+  icon: string;
+  label: string;
+}
+
+export const SnsIcon: FC<Props> = ({ href, icon, label }) => {
+  return (
+    <Link to={href}>
+      <Box w="35px" h="35px" borderRadius="50%">
+        <Image src={icon} alt={label} />
+      </Box>
+    </Link>
+  );
+};
