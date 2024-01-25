a2847a8b49db54a0175ca4a505277320eeeca550
kazutakanakamura711 <a711st777@gmail.com>
2024-01-25 23:02:32 +0900

    Add teamMemberStaff

---
+++ src/features/TeamMember/TeamMemberStaff/index.tsx
@@ -0,0 +1,117 @@
+import { MainLayout } from '@/components/Layouts/MainLayout';
+import { Box, Grid, Image, Text, VStack } from '@chakra-ui/react';
+import { FC } from 'react';
+
+interface TeamMemberStaff {
+  id: number;
+  name: string;
+  image: string;
+}
+
+const mainStaffs: TeamMemberStaff[] = [
+  {
+    id: 1,
+    name: 'KOHEI YAMAMOTO',
+    image: '/images/rider/img-top-rider-01.webp',
+  },
+  {
+    id: 2,
+    name: 'KOHEI YAMAMOTO',
+    image: '/images/rider/img-top-rider-01.webp',
+  },
+];
+const subStaffs: TeamMemberStaff[] = [
+  {
+    id: 1,
+    name: 'KOHEI YAMAMOTO',
+    image: '/images/rider/img-top-rider-01.webp',
+  },
+  {
+    id: 2,
+    name: 'KOHEI YAMAMOTO',
+    image: '/images/rider/img-top-rider-01.webp',
+  },
+  {
+    id: 3,
+    name: 'KOHEI YAMAMOTO',
+    image: '/images/rider/img-top-rider-01.webp',
+  },
+];
+
+interface SmallTitleProps {
+  title: string;
+}
+
+export const TeamMemberStaff = () => {
+  const SmallTitle: FC<SmallTitleProps> = ({ title }) => {
+    return (
+      <Box mb="92px" textAlign="center">
+        <Box color="#fff" fontSize="32px" fontWeight="bold">
+          {title}
+        </Box>
+      </Box>
+    );
+  };
+
+  return (
+    <MainLayout>
+      <Box mb="280px">
+        <SmallTitle title="The Staff" />
+        <VStack spacing={24}>
+          <Grid templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(2, 1fr)' }} gap={8}>
+            {mainStaffs.map(staff => (
+              <Box key={staff.id} position="relative">
+                <Box w={{base: '100%', lg: '72%'}} m="0 auto">
+                  <Image src={staff.image} />
+                </Box>
+                <Box
+                  position="absolute"
+                  bottom="0"
+                  left="50%"
+                  transform="translate(-50%, -50%)"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  w="100%"
+                >
+                  <Text fontSize="24px" mr="48px">
+                    {staff.name}
+                  </Text>
+                  <Box>
+                    <Image
+                      w="11px"
+                      h="25px"
+                      src="/images/ico-arrow-white-brock.svg"
+                    />
+                  </Box>
+                </Box>
+              </Box>
+            ))}
+          </Grid>
+
+          <Grid templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }} gap={{ base: 8, lg: 16 }}>
+            {subStaffs.map(staff => (
+              <Box key={staff.id} position="relative">
+                <Box w="100%" m="0 auto">
+                  <Image src={staff.image} />
+                </Box>
+                <Box
+                  position="absolute"
+                  bottom="0"
+                  left="50%"
+                  transform="translate(-50%, -50%)"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  w="100%"
+                >
+                  <Text fontSize="24px">{staff.name}</Text>
+                </Box>
+              </Box>
+            ))}
+          </Grid>
+        </VStack>
+      </Box>
+    </MainLayout>
+  );
+};
