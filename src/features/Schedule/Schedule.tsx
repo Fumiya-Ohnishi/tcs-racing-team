17b6b8092a062ae5dfad6431a20ff37b3634615a
kazutakanakamura711 <a711st777@gmail.com>
2024-01-31 02:59:22 +0900

    Add components and hooks

---
+++ src/features/Schedule/Schedule.tsx
@@ -0,0 +1,57 @@
+import { MainLayout } from '@/components/Layouts/MainLayout';
+import { Box, VStack } from '@chakra-ui/react';
+import { ScheduleItem } from './ScheduleItem';
+import { useSchedule } from './hooks/useSchedule';
+import { ScheduleTitle } from './components/ScheduleTitle';
+
+export interface ScheduleItem {
+  id: number;
+  title: string;
+  startDate?: string;
+  endDate?: string;
+  location?: string;
+}
+
+export const Schedule = () => {
+  const { raceScheduleList, eventScheduleList } = useSchedule();
+  if (!raceScheduleList || !eventScheduleList) return <Box>データがありません。</Box>;
+
+  return (
+    <Box mb="117px">
+      <MainLayout>
+        <Box display={{ base: 'block', lg: 'flex' }} gap="64px" mb="70px">
+          <Box flex="1" mb={{ base: '81px', lg: '0px' }}>
+            <ScheduleTitle text="Race" />
+            <VStack gap="24px" display="grid">
+              {raceScheduleList.map(item => (
+                <ScheduleItem
+                  key={item.id}
+                  url="#"
+                  title={item.title}
+                  startDate={item.startDate}
+                  endDate={item.endDate}
+                  location={item.location}
+                />
+              ))}
+            </VStack>
+          </Box>
+          <Box flex="1">
+            <ScheduleTitle text="Events" />
+            <VStack gap="24px" display="grid">
+              {eventScheduleList.map(item => (
+                <ScheduleItem
+                  key={item.id}
+                  url="#"
+                  title={item.title}
+                  startDate={item.startDate}
+                  endDate={item.endDate}
+                  location={item.location}
+                />
+              ))}
+            </VStack>
+          </Box>
+        </Box>
+      </MainLayout>
+    </Box>
+  );
+};
