56b8863d662077aa62d827a9c4a49982702aca5e
kazutakanakamura711 <a711st777@gmail.com>
2024-01-31 13:03:48 +0900

    Add url link

--- src/features/Top/Schedule/Schedule.tsx
+++ src/features/Top/Schedule/Schedule.tsx
@@ -2,20 +2,24 @@ import { MainLayout } from '@/components/Layouts/MainLayout';
 import { Box, VStack } from '@chakra-ui/react';
 import { ScheduleItem } from './ScheduleItem';
 import { LinkButton } from '@/components/Elements/LinkButton';
-import { useSchedule } from './hooks/useSchedule';
 import { ScheduleTitle } from './components/ScheduleTitle';
+import { useGetSchedule } from '@/shared/hooks/useGetSchedule';
 
 export interface ScheduleItem {
   id: number;
   title: string;
-  startDate?: string;
-  endDate?: string;
-  location?: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  url: string;
 }
 
+const limit = 6;
+
 export const Schedule = () => {
-  const { raceScheduleList, eventScheduleList } = useSchedule();
-  if (!raceScheduleList || !eventScheduleList) return <Box>データがありません。</Box>;
+  const { raceScheduleList, eventScheduleList } = useGetSchedule({ limit });
+  if (!raceScheduleList || !eventScheduleList)
+    return <Box>データがありません。</Box>;
 
   return (
     <Box mb="117px">
@@ -27,7 +31,7 @@ export const Schedule = () => {
               {raceScheduleList.map(item => (
                 <ScheduleItem
                   key={item.id}
-                  url="#"
+                  url={item.url}
                   title={item.title}
                   startDate={item.startDate}
                   endDate={item.endDate}
@@ -42,7 +46,7 @@ export const Schedule = () => {
               {eventScheduleList.map(item => (
                 <ScheduleItem
                   key={item.id}
-                  url="#"
+                  url={item.url}
                   title={item.title}
                   startDate={item.startDate}
                   endDate={item.endDate}
@@ -52,7 +56,7 @@ export const Schedule = () => {
             </VStack>
           </Box>
         </Box>
-        <LinkButton link="/staff" text="Overview" />
+        <LinkButton link="/schedule-page" text="Overview" />
       </MainLayout>
     </Box>
   );
