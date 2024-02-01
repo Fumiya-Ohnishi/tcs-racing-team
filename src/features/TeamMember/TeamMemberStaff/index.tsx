a7859d858b71947f44d7db4585aa1a433294cd47
kazutakanakamura711 <a711st777@gmail.com>
2024-02-01 21:09:37 +0900

    Fix

--- src/features/TeamMember/TeamMemberStaff/index.tsx
+++ src/features/TeamMember/TeamMemberStaff/index.tsx
@@ -2,7 +2,7 @@ import { MainLayout } from '@/components/Layouts/MainLayout';
 import { Box, Grid, Image, Text, VStack } from '@chakra-ui/react';
 import { FC } from 'react';
 import { TeamMemberSmallTitle } from '../components/TeamMemberSmallTitle';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 interface TeamMemberStaff {
   id: number;
@@ -12,35 +12,36 @@ interface TeamMemberStaff {
 
 const mainStaffs: TeamMemberStaff[] = [
   {
-    id: 1,
-    name: 'KOHEI YAMAMOTO',
+    id: 0,
+    name: '山本幸平',
     image: '/images/team/img-team-staff01.jpg',
   },
   {
-    id: 2,
-    name: 'KOHEI YAMAMOTO',
+    id: 1,
+    name: '福光悠佑',
     image: '/images/team/img-team-staff02.jpg',
   },
 ];
 const subStaffs: TeamMemberStaff[] = [
   {
-    id: 1,
-    name: 'KOHEI YAMAMOTO',
+    id: 0,
+    name: '佐藤寿美',
     image: '/images/team/img-team-staff03.jpg',
   },
   {
-    id: 2,
-    name: 'KOHEI YAMAMOTO',
+    id: 1,
+    name: '汐澤芳治',
     image: '/images/team/img-team-staff04.jpg',
   },
   {
-    id: 3,
-    name: 'KOHEI YAMAMOTO',
-    image: '/images/team/img-team-staff02.jpg',
+    id: 2,
+    name: 'Ms. X -Coming Soon',
+    image: 'https://placehold.jp/000000/000000/327x218.png',
   },
 ];
 
 export const TeamMemberStaff: FC = () => {
+  const navigate = useNavigate();
   return (
     <MainLayout>
       <Box mb={{ base: '200px', lg: '280px' }}>
@@ -51,44 +52,21 @@ export const TeamMemberStaff: FC = () => {
             gap={8}
           >
             {mainStaffs.map(staff => (
-              <Box key={staff.id} position="relative">
-                <Link to="/member-page">
-                  <Box w={{ base: '100%', lg: '72%' }} m="0 auto">
-                    <Image src={staff.image} />
-                  </Box>
-                  <Box
-                    position="absolute"
-                    bottom="0"
-                    left="50%"
-                    transform="translate(-50%, -50%)"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    w="100%"
-                  >
-                    <Text fontSize="24px" mr="48px">
-                      {staff.name}
-                    </Text>
-                    <Box>
-                      <Image
-                        w="11px"
-                        h="25px"
-                        src="/images/ico-arrow-white-brock.svg"
-                      />
-                    </Box>
-                  </Box>
-                </Link>
-              </Box>
-            ))}
-          </Grid>
-
-          <Grid
-            templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
-            gap={{ base: 8, lg: 16 }}
-          >
-            {subStaffs.map(staff => (
-              <Box key={staff.id} position="relative">
-                <Box w="100%" m="0 auto">
+              <Box
+                key={staff.id}
+                position="relative"
+                onClick={() => {
+                  navigate('/member-page', {
+                    state: { id: staff.id },
+                  });
+                }}
+              >
+                <Box
+                  w={{ base: '100%', lg: '72%' }}
+                  m="0 auto"
+                  bg="black"
+                  h="100%"
+                >
                   <Image src={staff.image} />
                 </Box>
                 <Box
@@ -101,7 +79,41 @@ export const TeamMemberStaff: FC = () => {
                   justifyContent="center"
                   w="100%"
                 >
-                  <Text fontSize="24px">{staff.name}</Text>
+                  <Text fontSize="18px" mr="48px">
+                    {staff.name}
+                  </Text>
+                  <Box>
+                    <Image
+                      w="11px"
+                      h="25px"
+                      src="/images/ico-arrow-white-brock.svg"
+                    />
+                  </Box>
+                </Box>
+              </Box>
+            ))}
+          </Grid>
+
+          <Grid
+            templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
+            gap={{ base: 8, lg: 16 }}
+          >
+            {subStaffs.map(staff => (
+              <Box key={staff.id} position="relative">
+                <Box w="100%" m="0 auto" bg="black" h="100%" minH="126px">
+                  <Image src={staff.image} />
+                </Box>
+                <Box
+                  position="absolute"
+                  bottom="0"
+                  left="50%"
+                  transform="translate(-50%, -50%)"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  w="100%"
+                >
+                  <Text fontSize="18px">{staff.name}</Text>
                 </Box>
               </Box>
             ))}
