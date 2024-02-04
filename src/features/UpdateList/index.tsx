19a09abaae7fe59c4ead6522490ad8bc6e129618
kazutakanakamura711 <a711st777@gmail.com>
2024-02-05 03:08:29 +0900

    Use UpdateListPageNation component

--- src/features/UpdateList/index.tsx
+++ src/features/UpdateList/index.tsx
@@ -2,13 +2,19 @@ import { Title } from '@/components/Elements/Title';
 import { PageLayout } from '@/components/Layouts/PageLayout';
 import { OurPartners } from '../Top/OurPartners';
 import { OfficialMedia } from '../Top/OfficialMedia';
+import { Box } from '@chakra-ui/react';
+import { UpdateListPageNation } from './UpdateListPageNation/UpdateListPagenation';
 
 export const UpdateListContent = () => {
   return (
     <PageLayout>
-      <Title title="Our Partners" subTitle="Our Partners" />
-      <OurPartners />
-      <OfficialMedia />
+      <Box pt="128px" mb="128px">
+        <Title title="Updates" subTitle="Updates" />
+      </Box>
+        <UpdateListPageNation />
+        <Title title="Our Partners" subTitle="Our Partners" />
+        <OurPartners />
+        <OfficialMedia />
     </PageLayout>
   );
 };
