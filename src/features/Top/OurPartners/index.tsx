7f587a3a0bc2846386788e6f4c2cda621308e656
kazutakanakamura711 <a711st777@gmail.com>
2025-02-05 20:22:21 +0900

    Fix

--- src/features/Top/OurPartners/index.tsx
+++ src/features/Top/OurPartners/index.tsx
@@ -28,7 +28,7 @@ export const OurPartners = () => {
           mb={{ base: '54px', lg: '84px' }}
           mx="auto"
         >
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="128px">
             <Image
               src="/images/top/ourPartners/img-sponsor-04.webp"
               alt="OurPartners"
@@ -36,7 +36,7 @@ export const OurPartners = () => {
               h="auto"
             />
           </Box>
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="80px">
             <Image
               src="/images/top/ourPartners/img-sponsor-05.webp"
               alt="OurPartners"
@@ -44,7 +44,7 @@ export const OurPartners = () => {
               h="auto"
             />
           </Box>
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="96px">
             <Image
               src="/images/top/ourPartners/img-sponsor-polygonbike.png"
               alt="OurPartners"
@@ -58,7 +58,7 @@ export const OurPartners = () => {
           mb={{ base: '54px', lg: '84px' }}
           mx="auto"
         >
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="88px">
             <Image
               src="/images/top/ourPartners/img-sponsor-02.webp"
               alt="OurPartners"
@@ -66,17 +66,17 @@ export const OurPartners = () => {
               h="auto"
             />
           </Box>
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="112px">
             <Image
-              src="/images/top/ourPartners/img-sponsor-06.webp"
+              src="/images/top/ourPartners/img-sponsor-07.webp"
               alt="OurPartners"
               w="100%"
               h="auto"
             />
           </Box>
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="96px">
             <Image
-              src="/images/top/ourPartners/img-sponsor-07.webp"
+              src="/images/top/ourPartners/img-sponsor-09.webp"
               alt="OurPartners"
               w="100%"
               h="auto"
@@ -87,9 +87,8 @@ export const OurPartners = () => {
           justifyContent="space-evenly" // 均等配置
           mb={{ base: '54px', lg: '84px' }}
           mx="auto"
-          w="33%"
         >
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="96px">
             <Image
               src="/images/top/ourPartners/img-sponsor-10.webp"
               alt="OurPartners"
@@ -99,7 +98,7 @@ export const OurPartners = () => {
           </Box>
         </HStack>
         <HStack justifyContent="space-evenly" mb={{ base: '54px', lg: '84px' }}>
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="152px">
             <Image
               src="/images/top/ourPartners/img-sponsor-12.webp"
               alt="OurPartners"
@@ -107,7 +106,7 @@ export const OurPartners = () => {
               h="auto"
             />
           </Box>
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="152px">
             <Image
               src="/images/top/ourPartners/img-sponsor-13.webp"
               alt="OurPartners"
@@ -115,7 +114,7 @@ export const OurPartners = () => {
               h="auto"
             />
           </Box>
-          <Box flex="1" maxW="148px">
+          <Box flex="1" maxW="88px">
             <Image
               src="/images/top/ourPartners/img-sponsor-03.webp"
               alt="OurPartners"
