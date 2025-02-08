af1b1cab1679a7418f49734d41c70eefb3317b49
kazutakanakamura711 <a711st777@gmail.com>
2025-02-08 23:55:16 +0900

    Fix file image path

--- src/constants/partnersImages.ts
+++ src/constants/partnersImages.ts
@@ -10,10 +10,10 @@ export type PartnersKey =
   | 'blisStage'
   | 'hokuei'
   | 'polygonBike'
-  | 'threePlmnt'
+  | 'naturalWin'
   | 'repetir'
   | 'yowapeda'
-  | 'revofish'
+  | 'revoFish'
   | 'kabuto'
   | 'hirooSangyo'
   | 'houseWs'
@@ -24,23 +24,23 @@ export type PartnersKey =
   | 'fox'
   | 'raceface'
   | 'proApparel'
-  | 'innoracks';
+  | 'inno';
 
 export const partners: Record<PartnersKey, Partner> = {
   alpenBlick: {
-    imagePath: '/images/top/ourPartners/img-sponsor-01.webp',
-    link: 'https://www.alpenblick-resort.com/',
+    imagePath: '/images/top/ourPartners/img-sponsor-alpenblick.webp',
+    link: 'https://www.alpenBlick-resort.com/',
     partnersPageLogoImageWidth: '',
     commonLogoImageWidth: '272px',
   },
   blisStage: {
-    imagePath: '/images/top/ourPartners/img-sponsor-04.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-blisStage.webp',
     link: 'https://www.blisstage.jp/',
     partnersPageLogoImageWidth: '112px',
     commonLogoImageWidth: '128px',
   },
   hokuei: {
-    imagePath: '/images/top/ourPartners/img-sponsor-05.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-hokuei.webp',
     link: 'https://www.hokuei-gr.com/',
     partnersPageLogoImageWidth: '64px',
     commonLogoImageWidth: '80px',
@@ -51,55 +51,55 @@ export const partners: Record<PartnersKey, Partner> = {
     partnersPageLogoImageWidth: '80px',
     commonLogoImageWidth: '96px',
   },
-  threePlmnt: {
-    imagePath: '/images/top/ourPartners/img-sponsor-02.webp',
+  naturalWin: {
+    imagePath: '/images/top/ourPartners/img-sponsor-naturalWin.webp',
     link: 'https://www.3plmnt.co.jp/',
     partnersPageLogoImageWidth: '72px',
     commonLogoImageWidth: '80px',
   },
   repetir: {
-    imagePath: '/images/top/ourPartners/img-sponsor-07.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-repetir.webp',
     link: 'https://repetir.official.ec/',
     partnersPageLogoImageWidth: '96px',
     commonLogoImageWidth: '104px',
   },
   yowapeda: {
-    imagePath: '/images/top/ourPartners/img-sponsor-09.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-yowapeda.webp',
     link: 'https://yowapeda.com/',
     partnersPageLogoImageWidth: '95px',
     commonLogoImageWidth: '96px',
   },
-  revofish: {
-    imagePath: '/images/top/ourPartners/img-sponsor-revofish.png',
+  revoFish: {
+    imagePath: '/images/top/ourPartners/img-sponsor-revoFish.png',
     link: 'https://www.revofish.com/',
     partnersPageLogoImageWidth: '64px',
     commonLogoImageWidth: '80px',
   },
   kabuto: {
-    imagePath: '/images/top/ourPartners/img-sponsor-10.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-kabuto.webp',
     link: 'https://www.ogkkabuto.co.jp/',
     partnersPageLogoImageWidth: '96px',
     commonLogoImageWidth: '112px',
   },
   hirooSangyo: {
-    imagePath: '/images/top/ourPartners/img-sponsor-13.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-hirooSangyo.webp',
     link: 'https://www.hiroosangyo.jp/',
     partnersPageLogoImageWidth: '150px',
     commonLogoImageWidth: '168px',
   },
   houseWs: {
-    imagePath: '/images/top/ourPartners/img-sponsor-03.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-watanabe.webp',
     link: 'https://house-ws.com/',
     partnersPageLogoImageWidth: '80px',
     commonLogoImageWidth: '80px',
   },
   beeSpeed: {
-    imagePath: '/images/top/ourPartners/img-sponsor-beespeed.png',
+    imagePath: '/images/top/ourPartners/img-sponsor-beeSpeed.png',
     link: 'https://www.beespeed.jp/',
     partnersPageLogoImageWidth: '152px',
   },
   shibaken: {
-    imagePath: '/images/top/ourPartners/img-sponsor-12.webp',
+    imagePath: '/images/top/ourPartners/img-sponsor-shibaken.webp',
     link: 'http://www.shibaken-inc.co.jp/',
     partnersPageLogoImageWidth: '152px',
   },
@@ -109,7 +109,7 @@ export const partners: Record<PartnersKey, Partner> = {
     partnersPageLogoImageWidth: '144px',
   },
   maxxis: {
-    imagePath: '/images/top/ourPartners/img-sponsor-11.jpg',
+    imagePath: '/images/top/ourPartners/img-sponsor-maxxis.jpg',
     link: 'https://www.maxxis.co.jp/',
     partnersPageLogoImageWidth: '112px',
   },
@@ -128,8 +128,8 @@ export const partners: Record<PartnersKey, Partner> = {
     link: 'https://www.proapparel.asia/',
     partnersPageLogoImageWidth: '104px',
   },
-  innoracks: {
-    imagePath: '/images/top/ourPartners/img-sponsor-08.webp',
+  inno: {
+    imagePath: '/images/top/ourPartners/img-sponsor-inno.webp',
     link: 'https://www.innoracks.com/',
     partnersPageLogoImageWidth: '112px',
   },
@@ -149,10 +149,10 @@ export const partnersImages: Record<PartnerCategory, Partner[]> = {
   black: [partners.alpenBlick],
   platinum: [partners.blisStage, partners.hokuei, partners.polygonBike],
   gold: [
-    partners.threePlmnt,
+    partners.naturalWin,
     partners.repetir,
     partners.yowapeda,
-    partners.revofish,
+    partners.revoFish,
   ],
   silver: [partners.kabuto],
   bronze: [partners.hirooSangyo, partners.houseWs],
@@ -163,6 +163,6 @@ export const partnersImages: Record<PartnerCategory, Partner[]> = {
     partners.fox,
     partners.raceface,
     partners.proApparel,
-    partners.innoracks,
+    partners.inno,
   ],
 };
